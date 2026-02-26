b7426cbb7a6ed8981f5ab57ec138bd0223e1b5e3
Surendra Singh Koranga <96525399+Surendrasingh6289@users.noreply.github.com>
2026-02-26 15:36:49 +0530

    Change model file extension from .sav to .pkl

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
     st.title("Diabetes Prediction")
     st.header("Machine Learning project",divider='red')
 
-    model = pickle.load("LogisticModel.sav")
+    model = pickle.load("LogisticModel.pkl")
 
 
     Pregnancy = st.number_input("Pregnancies",min_value=0,max_value=17)
